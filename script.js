159a3d849a837e3041101c78f17de008a4373f7c
jahphetB <japhetlakew098@gmail.com>
2026-08-27 21:37:16 -0600

    gameBoard in progress

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
-gameBoard(() => {
+const gameBoard = (() => {
     const board = [
         "empty", "empty", "empty",
         "empty", "empty", "empty", 
         "empty", "empty", "empty"
-    ]
+    ];
+
+    const getBoard = () => board; 
+
+    function placeMarker (position, marker) {
+        board [position] = marker; 
+    }
+
+    return {
+        getBoard, /** same as getBoard: "getBoard" */
+        placeMarker
+    };
 })();
